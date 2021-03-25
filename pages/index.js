d734d388eed90fd76c29a3f5f253f042edd1a276
Phil Liao <phil@0x.org>
2021-03-25 16:48:43 -0500

    update Title

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Token Pairs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
